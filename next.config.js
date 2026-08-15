02c7f9d0d6e39ab6a00904b92262f1a6a60b0789
notti-jp <kawashima.go@gmail.com>
2026-08-15 23:36:51 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      // /tutorial（末尾なし）でも、はじめてガイドを開けるようにする
+      { source: "/tutorial", destination: "/tutorial/index.html" },
+    ];
+  },
+};
 module.exports = nextConfig;
